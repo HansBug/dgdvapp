8d01861cdae349e9e48f6636fbb4c69032e0f0db
HansBug <hansbug@buaa.edu.cn>
2022-02-12 14:56:51 +0800

    dev(hansbug): complete the data source items

--- app/widget/form_generate.py
+++ app/widget/form_generate.py
@@ -1,8 +1,11 @@
 import os
+from types import MethodType
 from typing import List
 
-from PyQt5.Qt import QWidget, QInputDialog
-from types import MethodType
+import qtawesome as qta
+from PyQt5.Qt import QWidget, QInputDialog, QToolButton, QMenu, QAction, QPoint, Qt, QMessageBox, QTableWidgetItem
+from hbutils.string import plural_word
+
 from ..ui import UIFormGenerate
 from ..utils import smart_sort
 
@@ -17,6 +20,7 @@ class FormGenerate(QWidget, UIFormGenerate):
     def _init(self):
         self._init_perception()
         self._init_lost_possibility()
+        self._init_table_control_type()
 
     def _init_perception(self):
         def _edit_perception():
@@ -27,7 +31,12 @@ class FormGenerate(QWidget, UIFormGenerate):
                 items = smart_sort(filter(bool, map(str.strip, result_str.splitlines())))
                 self.edit_perception.setText(','.join(items))
 
-        self.button_edit_perception.clicked.connect(_edit_perception)
+        _button_edit = QToolButton(self)
+        _button_edit.setFixedHeight(self.edit_perception.height())
+        _button_edit.setFixedWidth(self.edit_initial_num.width() - self.edit_perception.width())
+        _button_edit.setText('...')
+        _button_edit.move(self.edit_perception.x() + self.edit_perception.width(), self.edit_perception.y())
+        _button_edit.clicked.connect(_edit_perception)
         self.edit_perception.mouseDoubleClickEvent = MethodType(lambda s, e: _edit_perception(), self)
 
     def _init_lost_possibility(self):
@@ -39,8 +48,241 @@ class FormGenerate(QWidget, UIFormGenerate):
                 items = smart_sort(filter(bool, map(str.strip, result_str.splitlines())))
                 self.edit_lost_possibility.setText(','.join(items))
 
-        self.button_edit_lost_possibility.clicked.connect(_edit_lost_possibility)
+        _button_edit = QToolButton(self)
+        _button_edit.setFixedHeight(self.edit_lost_possibility.height())
+        _button_edit.setFixedWidth(self.edit_initial_num.width() - self.edit_lost_possibility.width())
+        _button_edit.setText('...')
+        _button_edit.move(self.edit_lost_possibility.x() + self.edit_lost_possibility.width(),
+                          self.edit_lost_possibility.y())
+        _button_edit.clicked.connect(_edit_lost_possibility)
         self.edit_lost_possibility.mouseDoubleClickEvent = MethodType(lambda s, e: _edit_lost_possibility(), self)
+
+    def _init_table_control_type(self):
+        table = self.table_control_type
+        table.setColumnCount(2)
+        table.setProperty('data', [])
+
+        def _edit_item_time(row):
+            data: list = table.property('data')
+            item_str_lines = data[row]['time']
+            time_str, time_ok = QInputDialog.getMultiLineText(
+                self, 'Edit Item Time',
+                'Edit the times of control:', os.linesep.join(item_str_lines)
+            )
+
+            if time_ok:
+                time_items = smart_sort(set(filter(bool, map(str.strip, time_str.splitlines()))))
+
+                ti_time = QTableWidgetItem(','.join(map(str, time_items)))
+                ti_time.setFlags(Qt.ItemIsEnabled)
+                table.setItem(row, 0, ti_time)
+
+                data[row]['time'] = time_items
+                table.setProperty('data', data)
+
+        def _edit_item_control(row):
+            data: list = table.property('data')
+            control_lines = data[row]['control']
+            control_str, control_ok = QInputDialog.getMultiLineText(
+                self, 'Edit Item Control',
+                'Input the control items (gap,type,R1,R2,R3):',
+                os.linesep.join([','.join(ld) for ld in control_lines])
+            )
+
+            if control_ok:
+                control_lines = filter(bool, map(str.strip, control_str.splitlines()))
+                control_items = []
+                for lineno, line in enumerate(control_lines, 1):
+                    try:
+                        gap, type_, r1, r2, r3 = map(str.strip, line.split(',', maxsplit=4))
+                    except ValueError:
+                        QMessageBox.critical(
+                            self, 'Add Control Item',
+                            f'Invalid control as line {repr(lineno)} - {repr(line)}.'
+                        )
+                        return
+                    else:
+                        control_items.append((gap, type_, r1, r2, r3))
+
+                ti_control = QTableWidgetItem('(' + plural_word(len(control_items), 'command') + ')')
+                ti_control.setFlags(Qt.ItemIsEnabled)
+                table.setItem(row, 1, ti_control)
+
+                data[row]['control'] = control_items
+                table.setProperty('data', data)
+
+        def _edit_cell(row, column):
+            if column == 0:
+                _edit_item_time(row)
+            elif column == 1:
+                _edit_item_control(row)
+            else:
+                raise ValueError(f'Invalid column - {repr(column)}.')
+
+        table.cellDoubleClicked.connect(_edit_cell)
+
+        def _add_item(time_items, control_items):
+            row_cnt = table.rowCount()
+            data: list = table.property('data')
+            table.insertRow(row_cnt)
+
+            time_items = list(map(str, time_items))
+            control_items = [(str(gap), str(type_), str(r1), str(r2), str(r3))
+                             for gap, type_, r1, r2, r3 in control_items]
+
+            ti_time = QTableWidgetItem(','.join(map(str, time_items)))
+            ti_time.setFlags(Qt.ItemIsEnabled)
+            table.setItem(row_cnt, 0, ti_time)
+            ti_control = QTableWidgetItem('(' + plural_word(len(control_items), 'command') + ')')
+            ti_control.setFlags(Qt.ItemIsEnabled)
+            table.setItem(row_cnt, 1, ti_control)
+
+            data.insert(row_cnt, {
+                'time': time_items,
+                'control': control_items,
+            })
+            table.setProperty('data', data)
+            self.edit_control_num.setText(str(len(data)))
+
+        def _add_new_item():
+            time_str, time_ok = QInputDialog.getMultiLineText(self, 'Add Control Item', 'Input the times of control:')
+            if time_ok:
+                time_items = smart_sort(set(filter(bool, map(str.strip, time_str.splitlines()))))
+                control_str, control_ok = QInputDialog.getMultiLineText(
+                    self, 'Add Control Item',
+                    'Input the control items (gap,type,R1,R2,R3):'
+                )
+                if control_ok:
+                    control_lines = filter(bool, map(str.strip, control_str.splitlines()))
+                    control_items = []
+                    for lineno, line in enumerate(control_lines, 1):
+                        try:
+                            gap, type_, r1, r2, r3 = map(str.strip, line.split(',', maxsplit=4))
+                        except ValueError:
+                            QMessageBox.critical(
+                                self, 'Add Control Item',
+                                f'Invalid control as line {repr(lineno)} - {repr(line)}.'
+                            )
+                            return
+                        else:
+                            control_items.append((gap, type_, r1, r2, r3))
+
+                    _add_item(time_items, control_items)
+
+        def _del_one_item(item: QTableWidgetItem):
+            row = item.row()
+            data: list = table.property('data')
+            if QMessageBox.warning(
+                    self, 'Delete Control Item',
+                    f'Control item {repr(row)} will be removed, continue?',
+                    QMessageBox.Ok | QMessageBox.Cancel,
+            ) == QMessageBox.Ok:
+                data.pop(row)
+                table.removeRow(row)
+
+                table.setProperty('data', data)
+                self.edit_control_num.setText(str(len(data)))
+
+        def _clear_all_items():
+            data: list = table.property('data')
+            if QMessageBox.warning(
+                    self, 'Clear all items',
+                    f'All control items will be cleared, continue?',
+                    QMessageBox.Ok | QMessageBox.Cancel
+            ) == QMessageBox.Ok:
+                data.clear()
+                table.clear()
+
+                table.setProperty('data', data)
+                self.edit_control_num.setText(str(len(data)))
+
+        def _show_menu(curpos: QPoint):
+            menu = QMenu(table)
+            item = table.itemAt(curpos)
+
+            if item is not None:
+                row = item.row()
+                action_del_item = QAction(qta.icon('msc.remove'), f'&Delete control item {repr(row)}', menu)
+                action_del_item.triggered.connect(lambda: _del_one_item(item))
+                menu.addAction(action_del_item)
+
+                menu.addSeparator()
+
+            action_add_item = QAction(qta.icon('msc.add'), '&Add new control item', menu)
+            action_add_item.triggered.connect(_add_new_item)
+            menu.addAction(action_add_item)
+
+            action_clear = QAction(qta.icon('msc.clear-all'), '&Clear all items', menu)
+            action_clear.triggered.connect(_clear_all_items)
+            menu.addAction(action_clear)
+
+            dest_point = table.mapToGlobal(curpos)
+            menu.exec_(dest_point)
+
+        table.setContextMenuPolicy(Qt.CustomContextMenu)
+        table.customContextMenuRequested.connect(_show_menu)
+
+        _add_item(
+            [0],
+            [
+                (20, 1, 90, 0, 0),
+                (20, 2, 85, 40, 0),
+                (20, 4, 50, 300, 50),
+                (20, 4, 70, 80, 60),
+                (20, 5, 60, 100, 0),
+                (35, 3, 140, 100, 70),
+                (50, 1, 200, 0, 0),
+                (50, 2, 200, 150, 0),
+                (50, 3, 200, 150, 100),
+                (50, 3, 210, 200, 100),
+                (70, 2, 450, 200, 0),
+                (70, 3, 210, 200, 200),
+                (70, 5, 110, 300, 0),
+                (90, 1, 305, 0, 0),
+                (90, 2, 330, 300, 0),
+                (90, 4, 200, 210, 200),
+                (90, 4, 200, 400, 200),
+                (90, 5, 145, 400, 0),
+                (90, 5, 270, 270, 0),
+                (110, 1, 400, 0, 0),
+            ]
+        )
+        _add_item(
+            [45, 50, 60, 70, 75, 80, 100, 120],
+            [
+                (20, 0, 0, 0, 0),
+                (20, 1, 90, 0, 0),
+                (35, 0, 0, 0, 0),
+                (50, 0, 0, 0, 0),
+                (50, 1, 200, 0, 0),
+                (50, 2, 200, 150, 0),
+                (50, 3, 200, 150, 100),
+                (70, 0, 0, 0, 0),
+                (70, 2, 450, 200, 0),
+                (70, 3, 210, 200, 200),
+                (90, 0, 0, 0, 0),
+                (90, 1, 305, 0, 0),
+                (90, 2, 330, 300, 0),
+                (90, 4, 200, 210, 200),
+                (110, 1, 400, 0, 0),
+            ]
+        )
+
+    @property
+    def initial_num(self) -> str:
+        return self.edit_initial_num.text()
+
+    @property
+    def loc_offset(self) -> str:
+        return self.edit_loc_offset.text()
+
+    @property
+    def loc_err(self) -> str:
+        return self.edit_loc_err.text()
+
+    @property
+    def angle_err(self) -> str:
+        return self.edit_angle_err.text()
 
     @property
     def perceptions(self) -> List[str]:
