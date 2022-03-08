2781761561b0462aa3f4620c4a66f37f913e07cb
HansBug <hansbug@buaa.edu.cn>
2022-03-08 20:11:08 +0800

    release(hansbug): use version 0.1.4

--- app/config/meta.py
+++ app/config/meta.py
@@ -7,7 +7,7 @@ Overview:
 __TITLE__ = "dgdvapp"
 
 #: Version of this project.
-__VERSION__ = "0.1.3"
+__VERSION__ = "0.1.4"
 
 #: Short description of the project, will be included in ``setup.py``.
 __DESCRIPTION__ = 'Demo Application of PyQt5.'
