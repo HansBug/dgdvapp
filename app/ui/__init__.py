34033dad6d931b3a43f7d4017c4942aecafefbbb
HansBug <hansbug@buaa.edu.cn>
2022-08-03 22:23:27 +0800

    dev(hansbug): add a simple box with validation

--- app/ui/__init__.py
+++ app/ui/__init__.py
@@ -2,6 +2,7 @@ from .dialog_combo_ui import Ui_dialog_combo as UiDialogCombo
 from .dialog_config_ui import Ui_dialog_config as UIDialogConfig
 from .dialog_multiple_choice_ui import Ui_dialog_multiple_choice as UIDialogMultipleChoice
 from .form_anova_ui import Ui_form_anova as UIFormANOVA
+from .form_boxplot_ui import Ui_form_boxplot as UIFormBoxplot
 from .form_generate_ui import Ui_form_generate as UIFormGenerate
 from .form_log_process_ui import Ui_form_log_process as UIFormLogProcess
 from .form_message_logging_ui import Ui_form_message_logging as UIFormMessageLogging
@@ -9,4 +10,4 @@ from .form_metrics_ui import Ui_form_metrics as UIFormMetrics
 from .form_scatter_ui import Ui_form_scatter as UIFormScatter
 from .form_spearmanr_ui import Ui_form_spearmanr as UIFormSpearmanr
 from .main_window_ui import Ui_main_window as UIMainWindow
-from .form_boxplot_ui import Ui_form_boxplot as UIFormBoxplot
+from .widget_labeled_edit_ui import Ui_LabeledEdit as UILabeledEdit
---
+++ app/widget/widget_labeled_edit.py
@@ -0,0 +1,82 @@
+from PyQt5.Qt import QWidget
+from hbutils.color import Color
+
+from ..ui import UILabeledEdit
+
+
+class WidgetLabeledEdit(QWidget, UILabeledEdit):
+    def __init__(self, parent, label, init_value=None, placeholder=None):
+        QWidget.__init__(self, parent)
+        self.label = label
+        self.init_value = init_value
+        self.placeholder = placeholder
+
+        self.setupUi(self)
+        self._init()
+
+    def _init(self):
+        self._init_label()
+        self._init_content()
+        self._init_layout()
+
+    def _init_label(self):
+        self.edit_name.setText(self.label)
+
+    def _init_content(self):
+        if self.init_value is not None:
+            self.edit_content.setText(str(self.init_value))
+        if self.placeholder:
+            self.edit_content.setPlaceholderText(self.placeholder)
+
+        self._text_change_call()
+        self.edit_content.textChanged.connect(self._text_change_call)
+
+    def _init_layout(self):
+        content_geo = self.edit_content.geometry()
+        name_geo = self.edit_name.geometry()
+        width = content_geo.x() + content_geo.width() + name_geo.x()
+        height = content_geo.width() + content_geo.y() * 2
+        self.setFixedSize(width, height)
+
+    def _text_change_call(self):
+        try:
+            value = self._validate(self.edit_content.text())
+        except ValueError as err:
+            valid, value, (msg, *_) = False, None, err.args
+        else:
+            valid, msg = True, None
+
+        color = Color('black' if valid else 'red')
+        self.edit_name.setStyleSheet(f"QLabel {{ color: {color}; }} ")
+        self.edit_content.setStyleSheet(f"QLineEdit {{ color: {color}; }}")
+        if not valid:
+            self.edit_content.setToolTip(f'Validation Failed: {msg}' if not valid else '')
+        else:
+            self.edit_content.setToolTip(f'{self.label}: {value!r}')
+
+        self._after_changed()
+
+    @property
+    def value(self):
+        return self._validate(self.edit_content.text())
+
+    @value.setter
+    def value(self, newv):
+        self.edit_content.setText(str(self._validate(newv)))
+        self._text_change_call()
+
+    @property
+    def valid(self) -> bool:
+        try:
+            self._validate(self.edit_content.text())
+        except ValueError:
+            return False
+        else:
+            return True
+
+    # noinspection PyMethodMayBeStatic
+    def _validate(self, v):
+        return v
+
+    def _after_changed(self):
+        pass
